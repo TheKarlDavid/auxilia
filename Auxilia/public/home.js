433ab9cddd092ed0b9d218b8feaa1edc88ab206a
Karl David <68324556+TheKarlDavid@users.noreply.github.com>
2020-09-08 19:16:28 +0800

    15th commit

--- Auxilia/public/home.js
+++ Auxilia/public/home.js
@@ -1,26 +1,43 @@
 $(".drop").click(function() {
 	$(".leaf00").addClass("r");
+	$(".drop").addClass("c");
+	$(".textbox1").addClass("ct");
 });
-
 
 $(".drop2").click(function() {
-	$(".stem").addClass("rain");
-	$(".leaf00").addClass("leaf1_1");
-});
-
-
-$(".drop3").click(function() {
-	$(".stem").addClass("one");
-	
-	setTimeout(function () { 
-		$('.stem').removeClass('one');
-	}, 1200);  
+	$(".stem").addClass("rain2")
+	$(".leaf00").addClass("leaf1_2");
+	$(".leaf01").addClass("r");
+	$(".drop2").addClass("c2");
+	$(".textbox2").addClass("ct2");
 });
 
 $(".drop3").click(function() {
-	$(".stem").addClass("one");
-	
-	setTimeout(function () { 
-		$('.stem').removeClass('one');
-	}, 1200);  
+	$(".stem").addClass("rain3")
+	$(".leaf00").addClass("leaf1_3");
+	$(".leaf01").addClass("r2");
+	$(".leaf02").addClass("t");
+	$(".drop3").addClass("c3");
+	$(".textbox3").addClass("ct3");
+});
+
+$(".drop4").click(function() {
+	$(".stem").addClass("rain4")
+	$(".leaf00").addClass("leaf1_4");
+	$(".leaf01").addClass("r3");
+	$(".leaf02").addClass("t2");
+	$(".leaf03").addClass("y");
+	$(".drop4").addClass("c4");
+	$(".textbox4").addClass("ct4");
+});
+
+$(".drop5").click(function() {
+	$(".stem").addClass("rain5")
+	$(".leaf00").addClass("leaf1_5");
+	$(".leaf01").addClass("r4");
+	$(".leaf02").addClass("t3");
+	$(".leaf03").addClass("y2");
+	$(".leaf04").addClass("u");
+	$(".drop5").addClass("c5");
+	$(".textbox5").addClass("ct5");
 });
